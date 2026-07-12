7cdcac1c78b85ab0463aa927f59978cb81ef1dc1
dishantsaini25 <sainidishu2002@gmail.com>
2026-07-12 22:14:03 +0530

    All Done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,31 +18,35 @@ const reemKufi = Reem_Kufi({
   display: "swap",
 });
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${reemKufi.variable}`}>
-
       <head>
+        {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id=GTM-WJT5QXCX'+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WJT5QXCX');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TL4KD4P4');
           `}
         </Script>
       </head>
 
       <body className="relative min-h-screen bg-bg-light font-sans text-text-dark antialiased">
-
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WJT5QXCX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TL4KD4P4"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -60,19 +64,23 @@ export default function RootLayout({ children }) {
           className="fixed -right-40 top-20 z-0 h-[600px] w-[600px] rounded-full blur-3xl"
           style={{ background: "rgba(37, 99, 235, 0.08)" }}
         />
+
         <div
           className="fixed -left-40 bottom-20 z-0 h-[500px] w-[500px] rounded-full blur-3xl"
           style={{ background: "rgba(15, 184, 160, 0.07)" }}
         />
+
         <div
           className="fixed left-1/3 top-1/2 z-0 h-[400px] w-[400px] rounded-full blur-3xl"
           style={{ background: "rgba(34, 197, 94, 0.06)" }}
         />
 
         <Header />
+
         <main className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           {children}
         </main>
+
         <Footer />
         <WhatsAppButton />
       </body>
